c6b4b80e8f6895c76698dc38b1808ed48c3b6e1f
hibiki <devkimadore1228@gmail.com>
2022-09-20 19:56:10 +0900

    test migrate

--- db/migrates.py
+++ db/migrates.py
@@ -1,3 +1,4 @@
+from time import time
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -11,25 +12,42 @@ firebase_admin.initialize_app(cred, {
     }
 })
 
-##databaseに初期データを追加する
-users_ref = db.reference('/users')
+##databaseに初  期データを追加する
+# users_ref = db.reference('/users')
 
-users_ref.set({
-    'user001': {
-        'user_id': '1',
-        'name': 'unkonow'
-        },
-    'user002': {
-        'user_id': '2',
-        'name': 'tosabaka'
-        }
-    })
+# users_ref.set({
+#     'user001': {
+#         'user_id': '1',
+#         'name': 'unkonow'
+#         },
+#     'user002': {
+#         'user_id': '2',
+#         'name': 'tosabaka'
+#         }
+#     })
 
-# databaseにデータを追加する
-users_ref.child('user003').set({
-      'user_id': '3',
-      'name': 'ute'
-    })
+# # databaseにデータを追加する
+# users_ref.child('user003').set({
+#       'user_id': '3',
+#       'name': 'ute'
+#     })
+
+users_ref = db.reference('/diary')
+
+users_ref.set([
+    {
+        'body': '楽しかった',
+        'user_id': 1
+    },
+    {
+        'body': '嬉しい',
+        'user_id': 2
+    },
+    {
+        'body': 'ほんまカスやわ',
+        'user_id': 1
+    }
+])
 
 ##データを取得する
 print(users_ref.get())
