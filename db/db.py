50a4144cf19116a5a6f7acc5ae7bfcca97ee8999
hibiki <devkimadore1228@gmail.com>
2022-09-20 14:28:23 +0900

    make db

--- db/db.py
+++ db/db.py
@@ -12,11 +12,18 @@ engine = create_engine(
 Base = declarative_base()
 
 # Todoテーブルの定義
-class Todo(Base):
-    __tablename__ = 'todos'
+class Users(Base):
+    __tablename__ = 'users'
     id = Column('id', Integer, primary_key = True)
     user = Column('user', String(25))
+    
+
+class Diary(Base):
+    __tablename__ = 'diaries'
+    id = Column('id', Integer, primary_key = True)
+    user_id = Column('user_id', Integer)
     body = Column('body', String(200))
+
 
 # テーブル作成
 Base.metadata.create_all(bind=engine)
