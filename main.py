28f88ac33567e9292937f08e27be371ab0b9aaca
hibiki <devkimadore1228@gmail.com>
2022-09-21 12:15:09 +0900

    db ope完全に理解した

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import imp
+from unittest import async_case
 from fastapi import FastAPI, Depends
 # from routers.task import get_user
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from pydantic import BaseModel
 # from firebase import firebase
 # firebase = firebase.FirebaseApplication('https://pteracup-default-rtdb.firebaseio.com', None)
 # result = firebase.get('/users', None)
@@ -39,26 +41,58 @@ firebase_admin.initialize_app(cred, {
 diary_ref = db.reference('diary')
 users_ref = db.reference('users')
 
-
+class diary(BaseModel):
+    author: str
+    title: str
+    date: str
+    diaryText: str
+    
 app = FastAPI()
 
-@app.get("/login/")
+@app.get("/signup")
+async def signup(username:str, email:str, password:str):
+    result = users_ref.push({
+        'name': username,
+        'email': email,
+        'password': password
+    })
+    result = users_ref.get()
+
+    for key, val in result.items():
+        if key == "name":
+            if val == username:
+                return 
+    return 
+
+@app.get("/login")
 async def login():
     return
 
 @app.get("/diary/{user_id}")
 async def list(user_id:str):
     #ユーザーidが一致する日記をすべて持ってくる
-    result = users_ref.child('user004').get()
-    return  result
+    result = users_ref.get()
+    keys = []
+    vals = []
+    for key, val in result.items():
+        keys.append(key)
+        vals.append(val)
+        
+    return  vals['user_id']
 
-@app.get("/diary/{user_id}/{diary_id}")
+@app.get("/diary/{user_id}/{title}")
 async def select():
     return 
 
-@app.post("/diary/{user_id}/create")
-async def create():
-    return
+@app.get("/diary/create")
+async def create(user_id:int, body:str, title:str, date:str):
+    result = diary_ref.push({
+        'title': title,
+        'body': body,
+        'user_id': user_id,
+        'date': date
+    })
+    return {'msg': 'success!'}
 
 @app.post("/diary/{user_id}/{diary_id}/delete")
 async def delete():
